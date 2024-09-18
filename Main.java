fe1e8b1f03a88957744eb0ba1bd52a552c8801be
Socrates-gif <socratest3@nycstudents.net>
2024-09-17 23:15:56 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        public Feeder() {}
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+    }
+    public int getCurrentFood()
+    {
+        return currentFood;
+        System.out.println(f.getCurrentFood());
+    }
+    public Feeder(int c)
+    {
+        currentFood = c;
+        Feeder g = new Feeder(500);
+    }
+    public void simulateOneDay(int numBirds)
+    int random = (int) (Math.random() * 10 + 1));
+    System.out.println(Math.random() < 0.05);
+}
+
